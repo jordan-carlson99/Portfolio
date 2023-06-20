44698f92474e6e3b2c766d9d22b1fd583d79a53d
jordan-carlson99 <jordan.carlson.square@gmail.com>
2023-06-20 14:31:57 -0500

    fixed vite build error

--- src/components/ProjectList/ProjectList.jsx
+++ src/components/ProjectList/ProjectList.jsx
@@ -4,12 +4,20 @@ import { useState } from "react";
 
 export default function ProjectList({ projects }) {
   const [isHighlighted, setIsHighlighted] = useState(false);
-  const [cardOpen, setCardOpen] = useState(false)
+  const [cardOpen, setCardOpen] = useState(false);
 
   return (
     <div id={styles["project-container"]}>
       {projects.map((project) => {
-        return <ProjectCard project={...project} setIsHighlighted={setIsHighlighted} isHighlighted={isHighlighted} setCardOpen={setCardOpen} cardOpen={cardOpen} />;
+        return (
+          <ProjectCard
+            project={project}
+            setIsHighlighted={setIsHighlighted}
+            isHighlighted={isHighlighted}
+            setCardOpen={setCardOpen}
+            cardOpen={cardOpen}
+          />
+        );
       })}
     </div>
   );
