21fbdd99ebc044b6d1ec989aae9727f9cd55cf9b
jordan-carlson99 <jordan.carlson.square@gmail.com>
2023-06-25 15:25:44 -0500

    initialize Type Animation library

--- src/components/BioText/BioText.jsx
+++ src/components/BioText/BioText.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import styles from "./BioText.module.css";
 import ProjectList from "../ProjectList/ProjectList";
+import { TypeAnimation } from "react-type-animation";
 
 export default function BioText({ text, projects }) {
   const [terminalShrink, setTerminalShrink] = useState(false);
@@ -8,13 +9,8 @@ export default function BioText({ text, projects }) {
     (1.5 * text.split("\n").length - 0.2) * 1000
   );
   const [timer, setTimer] = useState(null);
-  // const [type, setType] = useState("bio-text");
-  // const typing = () => {
-  //   setType("bio-text typing");
-  // };
 
   useEffect(() => {
-    console.log(totalDelay);
     setTimer(
       setTimeout(() => {
         setTerminalShrink(true);
@@ -22,47 +18,48 @@ export default function BioText({ text, projects }) {
     );
   }, []);
 
-  let delay = -1.5;
-  let textArr = text.split("\n");
-  const paragraphs = textArr.map((element, index) => {
-    delay += 1.5;
-    if (element.length < 1) {
-      return null;
-    } else if (index == 0) {
-      element = "$ " + element;
-      return (
-        <p
-          key={index}
-          className={`${styles["bio-text"]} ${styles["typing"]}`}
-          style={{ animationDelay: delay + 0.3 + "s" }}
-        >
-          {element}
-        </p>
-      );
-    } else if (index == textArr.length - 1) {
-      element = "$ " + element;
-      return (
-        <p
-          key={index}
-          className={`${styles["bio-text"]} ${styles["typing"]}`}
-          style={{ animationDelay: delay - 0.5 + "s" }}
-        >
-          {element}
-        </p>
-      );
-    } else {
-      element = "> " + element;
-      return (
-        <p
-          key={index}
-          className={`${styles["bio-text"]} ${styles["typing"]}`}
-          style={{ animationDelay: delay + "s" }}
-        >
-          {element}
-        </p>
-      );
-    }
-  });
+  // let delay = -1.5;
+  // let textArr = text.split("\n");
+  // const paragraphs = textArr.map((element, index) => {
+  //   delay += 1.5;
+  //   if (element.length < 1) {
+  //     return null;
+  //   } else if (index == 0) {
+  //     element = "$ " + element;
+  //     return (
+  //       <p
+  //         key={index}
+  //         className={`${styles["bio-text"]} ${styles["typing"]}`}
+  //         style={{ animationDelay: delay + 0.3 + "s" }}
+  //       >
+  //         {element}
+  //       </p>
+  //     );
+  //   } else if (index == textArr.length - 1) {
+  //     element = "$ " + element;
+  //     return (
+  //       <p
+  //         key={index}
+  //         className={`${styles["bio-text"]} ${styles["typing"]}`}
+  //         style={{ animationDelay: delay - 0.5 + "s" }}
+  //       >
+  //         {element}
+  //       </p>
+  //     );
+  //   } else {
+  //     element = "> " + element;
+  //     return (
+  //       <p
+  //         key={index}
+  //         className={`${styles["bio-text"]} ${styles["typing"]}`}
+  //         style={{ animationDelay: delay + "s" }}
+  //       >
+  //         {element}
+  //       </p>
+  //     );
+  //   }
+  // });
+
   return (
     <div
       id={styles["bio-box"]}
@@ -85,7 +82,17 @@ export default function BioText({ text, projects }) {
             </p>
           </div>
         )}
-        {paragraphs}
+        <TypeAnimation
+          style={{
+            whiteSpace: "pre-line",
+            height: "195px",
+            display: "block",
+            color: "white",
+          }}
+          sequence={["cat ./bio.txt\n", 200, text, 200, ""]}
+          repeat={Infinity}
+          speed={70}
+        />
       </div>
       {terminalShrink && <ProjectList projects={projects} />}
     </div>
