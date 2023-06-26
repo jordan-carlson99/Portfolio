3d06fdb8a4a3a0de52be2e210735c360df4af794
jordan-carlson99 <jordan.carlson.square@gmail.com>
2023-06-26 13:09:46 -0500

    added icons

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -14,16 +14,52 @@ export default function Header() {
       ></div>
       <div id={styles["info"]}>
         <h1 id={styles["name"]}>Jordan Carlson</h1>
+        <div id={styles["bar"]}></div>
         <div id={styles["skills-section"]}>
           <p className={styles["skills"]}>
-            Front end - JavaScript | React | HTML | CSS | Bootstrap |
+            Languages: JavaScript | HTML | CSS | SQL
           </p>
           <p className={styles["skills"]}>
-            Back end - Node.js | Express | PostgreSQL | Docker | MySQL |RESTful
-            APIs | MVCs
+            Libraries & Frameworks: Node | Express | React | Vite | Bootstrap |
+            Jquery
+          </p>
+          <p className={styles["skills"]}>
+            Tools: Git | Docker | pgbench | K6 | Node | Playwright
           </p>
         </div>
       </div>
+      {!window.matchMedia("(max-width: 450px)").matches && (
+        <div id={styles["contact-btn-container"]}>
+          <a
+            className={styles["contact-btn"]}
+            target="_blank"
+            href="https://github.com/jordan-carlson99"
+          >
+            <img className={styles["contact-img"]} src="./github.png"></img>
+          </a>
+          <a
+            className={styles["contact-btn"]}
+            target="_blank"
+            href="https://www.linkedin.com/in/jordan-carlson99/"
+          >
+            <img className={styles["contact-img"]} src="./linkedin.png"></img>
+          </a>
+          <a
+            className={styles["contact-btn"]}
+            target="_blank"
+            href="mailto: jordan.carlson.square@gmail.com"
+          >
+            <img className={styles["contact-img"]} src="./email.png"></img>
+          </a>
+          <a
+            className={styles["contact-btn"]}
+            target="_blank"
+            href="https://github.com/jordan-carlson99"
+          >
+            <img className={styles["contact-img"]} src="./resume.png"></img>
+          </a>
+        </div>
+      )}
     </header>
   );
 }
